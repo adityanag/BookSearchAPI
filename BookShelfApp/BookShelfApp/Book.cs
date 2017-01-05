60be4c8c2fb9e25a2b7ab06fd5c500ff2ac76b0a
Aditya Nag <adityanag@gmail.com>
2017-01-05 12:29:44 -0500

    Made the search more general, instead of ISBN only. Also added properties to the book model, and am filling in all the properties from json and printing them.

--- BookShelfApp/BookShelfApp/Book.cs
+++ BookShelfApp/BookShelfApp/Book.cs
@@ -8,7 +8,14 @@ namespace BookShelfApp
 {
     class Book
     {
+        public string ISBN { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string ImageLink { get; set; }
+
     }
 }
--- BookShelfApp/BookShelfApp/Program.cs
+++ BookShelfApp/BookShelfApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -15,31 +16,45 @@ namespace BookShelfApp
         static void Main(string[] args)
         {
             
-            Console.WriteLine("*** Let's search the Google Books API with an ISBN ***");
-            Console.Write("Enter ISBN Number: ");
-            string isbn = Console.ReadLine();
+            Console.WriteLine("*** Let's search the Google Books API with a search term ***");
+            Console.Write("Enter Search Term: ");
+            string searchTerm = Console.ReadLine();
             //string isbn = "1933354313";
 
-            string jsonResponse = SearchGoogleBooks(isbn);
+            string jsonResponse = SearchGoogleBooks(searchTerm);
 
             JObject json = JObject.Parse(jsonResponse);
 
             Book newBook = new Book();
-            newBook.Author = (string)json.SelectToken("items[0].volumeInfo.authors[0]");
+            newBook.ISBN = (string)json.SelectToken("items[0].volumeInfo.industryIdentifiers[0].identifier");
             newBook.Title = (string)json.SelectToken("items[0].volumeInfo.title");
+            newBook.Author = (string)json.SelectToken("items[0].volumeInfo.authors[0]");
+            newBook.Publisher = (string)json.SelectToken("items[0].volumeInfo.publisher");
+            newBook.PublishedDate = (string)json.SelectToken("items[0].volumeInfo.publishedDate");
+            newBook.Description = (string)json.SelectToken("items[0].volumeInfo.description");
+            newBook.Category = (string)json.SelectToken("items[0].volumeInfo.categories[0]");
+            newBook.ImageLink = (string)json.SelectToken("items[0].volumeInfo.imageLinks.thumbnail");
 
-            Console.WriteLine("Title: {0}", newBook.Title);
-            Console.WriteLine("Author: {0}", newBook.Author);
+            //This next loop prints all properties of the object newBook. Useful to see what's in an object!
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(newBook))
+            {
+                string name = descriptor.Name;
+                object value = descriptor.GetValue(newBook);
+                Console.WriteLine("{0} = {1}", name, value);
+            }
+
+            
             Console.WriteLine("Press any key to end");
             Console.ReadKey();
 
         }
 
-        private static string SearchGoogleBooks(string isbn)
+        //This method searches the GoogleBooks API with a given search term
+        private static string SearchGoogleBooks(string searchTerm)
         {
             string GoogleBooksApiKey = "AIzaSyBaf_7wmQ6Ve26VpFEocW7YHQCoRFV3prQ";
 
-            WebRequest request = WebRequest.Create("https://www.googleapis.com/books/v1/volumes?q=+isbn:" + isbn + "&key=" + GoogleBooksApiKey);
+            WebRequest request = WebRequest.Create("https://www.googleapis.com/books/v1/volumes?q=" + searchTerm + "&key=" + GoogleBooksApiKey);
             WebResponse response = request.GetResponse();
             // Display the status.
             //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
