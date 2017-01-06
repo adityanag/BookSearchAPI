e483c960a1956b397e6d82db7c5ff0aa194dd9a4
Aditya Nag <adityanag@gmail.com>
2017-01-06 03:24:09 -0500

    Displays a list of search results now. Also checks to see how many (upto a max of 20)

--- BookShelfApp/BookShelfApp/Book.cs
+++ BookShelfApp/BookShelfApp/Book.cs
@@ -4,6 +4,23 @@ namespace BookShelfApp
 {
     class Book
     {
+        public Book(){}
+
+
+        //Constructor so I can use the object initializer syntax in the for loop.
+        public Book(string isbn, string title, string author, string publisher, string publisheddate, string description, string category, string imagelink)
+        {
+            ISBN = isbn;
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            PublishedDate = publisheddate;
+            Description = description;
+            Category = category;
+            ImageLink = imagelink;
+
+        }
+
         public string ISBN { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
--- BookShelfApp/BookShelfApp/Program.cs
+++ BookShelfApp/BookShelfApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Net;
@@ -11,46 +12,79 @@ namespace BookShelfApp
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("*** Let's search the Google Books API with a search term ***");
+            int totalItems = 0; //set totalItems to 0 to start
+
+            Console.WriteLine("--------- Let's search the Google Books API with a search term ----------");
             Console.Write("Enter Search Term: ");
             string searchTerm = Console.ReadLine();
-            
-
             string jsonResponse = SearchGoogleBooks(searchTerm);
 
             JObject json = JObject.Parse(jsonResponse);
 
-            Book newBook = new Book();
-            newBook.ISBN = (string)json.SelectToken("items[0].volumeInfo.industryIdentifiers[0].identifier");
-            newBook.Title = (string)json.SelectToken("items[0].volumeInfo.title");
-            newBook.Author = (string)json.SelectToken("items[0].volumeInfo.authors[0]");
-            newBook.Publisher = (string)json.SelectToken("items[0].volumeInfo.publisher");
-            newBook.PublishedDate = (string)json.SelectToken("items[0].volumeInfo.publishedDate");
-            newBook.Description = (string)json.SelectToken("items[0].volumeInfo.description");
-            newBook.Category = (string)json.SelectToken("items[0].volumeInfo.categories[0]");
-            newBook.ImageLink = (string)json.SelectToken("items[0].volumeInfo.imageLinks.thumbnail");
-
-            //This next loop prints all properties of the object newBook. Useful to see what's in an object!
-            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(newBook))
+            if ((JArray)json["items"]!= null) //If there are Items, then set the number in the totalItems variable
             {
-                string name = descriptor.Name;
-                object value = descriptor.GetValue(newBook);
-                Console.WriteLine("{0} = {1}", name, value);
+                JArray items = (JArray)json["items"];
+                 totalItems = items.Count;
+            }
+            
+            //If we didn't get any results, say that and exit. If we actually did get results, then process
+            if (totalItems == 0)
+            {
+                Console.WriteLine("No results found");
+                
             }
 
+            else
+            {
+                List<Book> books = new List<Book>(); //create a list of books
+
+                //The loop runs as many times as there are items. Max will be 20, since I'm forcing only 20 results to be returned
+                // with maxResults in the search parameter string.
+                for(int i = 0; i<totalItems;i++)
+                {
+                    //This adds each new book object with the object initialization syntax and the custom constructor. I'm using string interpolation (the $ before the string)
+                    //to get the variable "i". Right now, I'm only pulling the first author name and the first category, hence the hardcoded 0 for those values.
+                    books.Add(new Book(
+                        (string)json.SelectToken($"items[{i}].volumeInfo.industryIdentifiers[0].identifier"),
+                        (string)json.SelectToken($"items[{i}].volumeInfo.title"),
+                        (string)json.SelectToken($"items[{i}].volumeInfo.authors[0]"),
+                        (string)json.SelectToken($"items[{i}].volumeInfo.publisher"),
+                        (string)json.SelectToken($"items[{i}].volumeInfo.publishedDate"),
+                        (string)json.SelectToken($"items[{i}].volumeInfo.description"),
+                        (string)json.SelectToken($"items[{i}].volumeInfo.categories[0]"),
+                        (string)json.SelectToken($"items[{i}].volumeInfo.imageLinks.thumbnail")
+                        ));
+                }
+
+                int count = 1; //initialize a counter to tell the user
+                //Nested loops - for each book, print every property name and value. It will print the entire list.
+                foreach (var book in books)
+                {
+                    
+                    Console.WriteLine("Result {0}", count);
+                    foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(book))
+                    {
+                        string name = descriptor.Name;
+                        object value = descriptor.GetValue(book);
+                        Console.WriteLine("{0} = {1}", name, value);
+                    }
+                    Console.WriteLine("--------------------------------------"); //separator for easy reading
+                    count++; // keep count from 1 -> whatever the totalItems is, with a max of 20 of course.
+                }
+
+            }
             
             Console.WriteLine("Press any key to end");
             Console.ReadKey();
 
         }
 
-        //This method searches the GoogleBooks API with a given search term
+        //This method searches the GoogleBooks API with a given search term. Only 20 results will be returned.
         private static string SearchGoogleBooks(string searchTerm)
         {
             string GoogleBooksApiKey = "AIzaSyCy4jqTONhRa3GXaZIfRMs1KJ4KE8Gjr-8";
-
-            WebRequest request = WebRequest.Create("https://www.googleapis.com/books/v1/volumes?q=" + searchTerm + "&key=" + GoogleBooksApiKey);
+            //maxResults is set to 20 so that we only get a small list
+            WebRequest request = WebRequest.Create("https://www.googleapis.com/books/v1/volumes?q=" + searchTerm +"&maxResults=20&orderBy=relevance&projection=full&key=" + GoogleBooksApiKey);
             WebResponse response = request.GetResponse();
             // Display the status.
             //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
